8a10a80c7a6f6f9e4a3f3b804978172ef802df74
lightxlamp <sheveliukhin.s@gmail.com>
2021-03-18 03:14:49 +0600

    docs: play with JSX

--- 01_first_proj/my-react-app/src/App.js
+++ 01_first_proj/my-react-app/src/App.js
@@ -3,20 +3,37 @@ import './App.css';
 
 function App() {
   return (
+    // <div>Can't return more than 1 node</div> // Adjacent JSX elements must be wrapped in an enclosing tag.
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey
         </p>
+        {/* <a>  SyntaxError: my-react-app/src/App.js: Expected corresponding JSX closing tag for <a> (26:6)*/}
         <a
-          className="App-link"
+          class="testWarning" // Warning: Invalid DOM property `class`. Did you mean `className`? 
+          // at a
+          // at header
+          // at div
+          // at App
+          className="App-link" // we are not in HTML, so write className not just class
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <a
+          className="App-link"
+          href="https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel">
+          What is "rel" attribute 
+        </a>
+        <ul>
+          <li>JS</li>
+          <li>CSS</li>
+          <li>HTML</li>
+        </ul>
       </header>
     </div>
   );
