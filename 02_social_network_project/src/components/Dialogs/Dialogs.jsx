11a3ab0b49f7a70e44ba76e442bd615260baaefd
lightxlamp <sheveliukhin.s@gmail.com>
2021-03-22 03:17:51 +0600

    feat: change structure of "Dialogs" page

--- 02_social_network_project/src/components/Dialogs/Dialogs.jsx
+++ 02_social_network_project/src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,24 @@ import s from './Dialogs.module.scss'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+           <ul className={s.dialogs__list}>
+               <li className={s.dialogs__item}>Matteo</li>
+               <li className={s.dialogs__item}>Erick</li>
+               <li className={s.dialogs__item + ' ' + s.active }>James</li>
+               <li className={s.dialogs__item}>Anmary</li>
+               <li className={s.dialogs__item}>Janet</li>
+               <li className={s.dialogs__item}>Tilek</li>
+               <li className={s.dialogs__item}>Mike</li>
+               <li className={s.dialogs__item}>Samuel</li>
+           </ul>
+           <ul className={s.messages}>
+                <li className={s.message}>Morning</li>
+                <li className={s.message}>Call in 5 mins?</li>
+                <li className={s.message}>OK</li>
+           </ul>
         </div>
+        // Testing two different naming approaches. Will see what code looks better
     )
 }
 
